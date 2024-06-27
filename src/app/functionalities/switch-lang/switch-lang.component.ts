12bd6aeb7280008bee8e3a11335da40bd0568848
monix <mhs.dev.data@gmail.com>
2024-06-28 00:26:09 +0200

    Nuevo componente para seleccionar idiomas

---
+++ src/app/functionalities/switch-lang/switch-lang.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-switch-lang',
+  standalone: true,
+  imports: [],
+  templateUrl: './switch-lang.component.html',
+  styleUrl: './switch-lang.component.css'
+})
+export class SwitchLangComponent {
+
+}
