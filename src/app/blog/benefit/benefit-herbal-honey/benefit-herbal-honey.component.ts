fbf27f294c67d58cb327ac0495369291627ecd2d
monix <mhs.dev.data@gmail.com>
2024-06-28 14:17:15 +0200

    nuevo componente benefitHerbalHoney

---
+++ src/app/blog/benefit/benefit-herbal-honey/benefit-herbal-honey.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-benefit-herbal-honey',
+  standalone: true,
+  imports: [],
+  templateUrl: './benefit-herbal-honey.component.html',
+  styleUrl: './benefit-herbal-honey.component.css'
+})
+export class BenefitHerbalHoneyComponent {
+
+}
