7badfb72a56b6ef7da91c36a42807428756b54a8
monix <mhs.dev.data@gmail.com>
2024-06-28 14:18:14 +0200

    contenido del conponente benefitHerbalHoney

--- src/app/blog/benefit/benefit-herbal-honey/benefit-herbal-honey.component.ts
+++ src/app/blog/benefit/benefit-herbal-honey/benefit-herbal-honey.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-benefit-herbal-honey',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    TranslateModule
+  ],
   templateUrl: './benefit-herbal-honey.component.html',
   styleUrl: './benefit-herbal-honey.component.css'
 })
 export class BenefitHerbalHoneyComponent {
-
+  constructor(
+    public translate: TranslateService
+  ){}
 }
