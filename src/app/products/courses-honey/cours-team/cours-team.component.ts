927b8aaebb777b7ab4280f8885a9205ce8f20562
monix <mhs.dev.data@gmail.com>
2024-06-29 20:12:04 +0200

    añadir el compoente team cours

---
+++ src/app/products/courses-honey/cours-team/cours-team.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cours-team',
+  standalone: true,
+  imports: [],
+  templateUrl: './cours-team.component.html',
+  styleUrl: './cours-team.component.css'
+})
+export class CoursTeamComponent {
+
+}
