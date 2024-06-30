936e9585c043140a57c30d699f225e21c970bf3a
monix <mhs.dev.data@gmail.com>
2024-06-30 19:12:46 +0200

    Aumtentar el contenido del About con los nuevos componentes

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { PoliciesComponent } from './policies/policies.component';
 import { DeliveryComponent } from './delivery/delivery.component';
+import { WhereareComponent } from './whereare/whereare.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
   imports: [
     PoliciesComponent,
-    DeliveryComponent
+    DeliveryComponent,
+    WhereareComponent
   ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
--- src/app/about/whereare/whereare.component.ts
+++ src/app/about/whereare/whereare.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-whereare',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    TranslateModule
+  ],
   templateUrl: './whereare.component.html',
   styleUrl: './whereare.component.css'
 })
 export class WhereareComponent {
+  constructor(
+    public translate: TranslateService
+  ) {}
 
 }
