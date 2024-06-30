e8774cabe382b69833f306377a0b109f2fb99029
monix <mhs.dev.data@gmail.com>
2024-06-30 19:04:52 +0200

    Nuevo componente WhereAre

---
+++ src/app/whereare/whereare.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-whereare',
+  standalone: true,
+  imports: [],
+  templateUrl: './whereare.component.html',
+  styleUrl: './whereare.component.css'
+})
+export class WhereareComponent {
+
+}
