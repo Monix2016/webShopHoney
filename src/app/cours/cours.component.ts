7fa90c1db091a0841bf8967b3ebc8799ab493017
monix <mhs.dev.data@gmail.com>
2024-06-30 16:25:11 +0200

    incluir la lista de cursos el team en l componente coursos

--- src/app/cours/cours.component.ts
+++ src/app/cours/cours.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, inject, input } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { CoursesHoneyComponent } from '../products/courses-honey/courses-honey.component';
+import { ICours } from '../interfaces/i-courses';
+import { HousingService } from '../services/housing.service';
+import { ITeam } from '../interfaces/i-team';
+import { CoursTeamComponent } from '../products/courses-honey/cours-team/cours-team.component';
 
 @Component({
   selector: 'app-cours',
   standalone: true,
-  imports: [],
+  imports: [
+    TranslateModule,
+    CommonModule,
+    CoursesHoneyComponent,
+    CoursTeamComponent
+  ],
   templateUrl: './cours.component.html',
   styleUrl: './cours.component.css'
 })
 export class CoursComponent {
+  coursesHoneyList:ICours[]=[];
+  team: ITeam[] = [];
+  housingService: HousingService = inject(HousingService);
 
+  constructor(public translate: TranslateService){
+    this.coursesHoneyList= this.housingService.getAllCourses();
+    this.team = this.housingService.getTeam();
+  }
 }
