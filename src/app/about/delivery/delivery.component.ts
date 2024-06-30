09ec8846262ca4803e7f4f59e36e2c184508d8d2
monix <mhs.dev.data@gmail.com>
2024-06-30 18:29:53 +0200

    Contenido del componente Delivery

--- src/app/about/delivery/delivery.component.ts
+++ src/app/about/delivery/delivery.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-delivery',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    TranslateModule
+  ],
   templateUrl: './delivery.component.html',
   styleUrl: './delivery.component.css'
 })
 export class DeliveryComponent {
+  constructor(
+    public translate: TranslateService
+  ) {}
 
 }
