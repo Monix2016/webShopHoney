d3730a3a91f3bd69f0e0fd653814cc978edfaf4b
monix <mhs.dev.data@gmail.com>
2024-07-06 19:33:39 +0200

    Crear el Botón "Añadir al Carrito" con el componente AddCart y la interfaz del articulo a añadir al carrito

--- src/app/functionalities/add-cart/add-cart.component.ts
+++ src/app/functionalities/add-cart/add-cart.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { IArticle } from '../../interfaces/i-article';
+import { CartService } from '../../services/cart.service';
+import { CommonModule } from '@angular/common';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-add-cart',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    TranslateModule
+  ],
   templateUrl: './add-cart.component.html',
   styleUrl: './add-cart.component.css'
 })
 export class AddCartComponent {
+  products: IArticle[]= [
+    // Ejemplo de productos, deberías cargar esto desde una API o servicio
+    { id: 1, name: 'Miel', description: 'Miel pura de abeja', price: 10, image: 'path-to-image' }
+  ];
+
+
+  constructor(
+    private cartService: CartService,
+    public translate: TranslateService
+  ) {}
+
+  addToCart(product: IArticle): void {
+    this.cartService.addToCart({
+      id: product.id,
+      name: product.name,
+      price: product.price,
+      quantity: 1
+    });
+  }
 
 }
