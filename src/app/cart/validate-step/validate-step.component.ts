6f3c69cb77a6062341278a601562e40108130d8f
monix <mhs.dev.data@gmail.com>
2024-07-07 23:28:42 +0200

    mostrar la lsita de articulos modo lectura

--- src/app/cart/validate-step/validate-step.component.ts
+++ src/app/cart/validate-step/validate-step.component.ts
@@ -1,22 +1,32 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { ICartitem } from '../../interfaces/i-cartitem';
+import { CartService } from '../../services/cart.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-validate-step',
   standalone: true,
   imports: [
-    TranslateModule
+    TranslateModule,
+    CommonModule
   ],
   templateUrl: './validate-step.component.html',
   styleUrl: './validate-step.component.css'
 })
-export class ValidateStepComponent {
+export class ValidateStepComponent implements OnInit  {
   @Output() nextStep = new EventEmitter<void>();
+  cartItems: ICartitem[] = [];
 
   constructor(
-    public translate: TranslateService
+    public translate: TranslateService,
+    private cartService: CartService
   ){}
 
+
+  ngOnInit(): void {
+    this.cartService.getCartItems().subscribe(items => this.cartItems = items);
+  }
   next(): void {
     this.nextStep.emit();
   }
