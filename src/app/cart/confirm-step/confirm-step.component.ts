032e74b54b99a07544f4b3d49ee3228d55c2d6fe
monix <mhs.dev.data@gmail.com>
2024-07-09 13:36:35 +0200

    preparar el componente confitmacion y recuperar los datos recogido desde el componente "Personal-info"

--- src/app/cart/confirm-step/confirm-step.component.ts
+++ src/app/cart/confirm-step/confirm-step.component.ts
@@ -1,20 +1,38 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { IPersonalInfo } from '../../interfaces/i-personal-info';
+import { CartService } from '../../services/cart.service';
+import { PersonalInfoService } from '../../services/personal-info.service';
 
 @Component({
   selector: 'app-confirm-step',
   standalone: true,
   imports: [
     TranslateModule,
+    CommonModule
   ],
   templateUrl: './confirm-step.component.html',
   styleUrl: './confirm-step.component.css'
 })
-export class ConfirmStepComponent {
+export class ConfirmStepComponent implements OnInit {
   @Output() nextStep = new EventEmitter<void>();
+  personalInfo!: IPersonalInfo;
+  cartItems: any[] = [];
+  totalPrice: number = 0;
+
   constructor(
-    public translate: TranslateService
-  ){}
+    public translate: TranslateService,
+    private cartService: CartService,
+    private personalInfoService: PersonalInfoService,
+  ) { }
+
+
+  ngOnInit(): void {
+    this.personalInfo = this.personalInfoService.getPersonalInfo();
+    this.cartService.getCartItems().subscribe(items => this.cartItems = items);
+    this.totalPrice = this.cartService.getTotalPrice();
+  }
 
   next(): void {
     this.nextStep.emit();
