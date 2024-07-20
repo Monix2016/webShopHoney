f25c1063cb4aae551807def7d7454cb5fff8486b
monix <mhs.dev.data@gmail.com>
2024-07-20 20:38:07 +0200

    taducciones

--- src/app/products/courses-honey/courses-honey.component.ts
+++ src/app/products/courses-honey/courses-honey.component.ts
@@ -2,12 +2,14 @@ import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ICours } from '../../interfaces/i-courses';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-courses-honey',
   standalone: true,
   imports: [
     CommonModule,
+    TranslateModule,
     RouterModule
   ],
   templateUrl: './courses-honey.component.html',
