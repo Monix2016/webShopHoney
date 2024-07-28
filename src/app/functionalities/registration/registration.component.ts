59f4214bc35c056e3e44cb3d10aa1d028846ce55
monix <mhs.dev.data@gmail.com>
2024-07-28 21:27:07 +0200

    el contenido del componente registration

--- src/app/functionalities/registration/registration.component.ts
+++ src/app/functionalities/registration/registration.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { ContactUsComponent } from '../../about/contact-us/contact-us.component';
+import { FollowUsComponent } from '../../about/follow-us/follow-us.component';
+import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    TranslateModule,
+    ContactUsComponent,
+    FollowUsComponent
+  ],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.css'
 })
