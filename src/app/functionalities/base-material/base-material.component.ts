e7faae21f34b81a3c1a7ce5b9ecfbb7c01904e5a
monix <mhs.dev.data@gmail.com>
2024-08-04 23:31:14 +0200

    añadir un componete comun para el  material

---
+++ src/app/functionalities/base-material/base-material.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-base-material',
+  standalone: true,
+  imports: [],
+  templateUrl: './base-material.component.html',
+  styleUrl: './base-material.component.css'
+})
+export class BaseMaterialComponent {
+
+}
